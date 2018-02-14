b463e0e17bee08c2b91c0a8ec08f8be5a2d9e33a
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-02-14 13:24:42 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# The world of quotes
+
+This is  a Pen created at CodePen.io. You can find this one at https://codepen.io/minimarz/pen/ZrJLVM.
+
+ Use: Get random quotes that you can share to your twitter.
---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+  $.getJSON(
+    "https://gist.githubusercontent.com/mjanavic/3172b4f732eb8970fefa9ebd16149cfb/raw/17a539f37fcaf5b47547e6ea5a0a85e674ca1e09/quotes.json",
+    function (data) {
+      $("#bquote").on("click", function () {
+        var randomNum = Math.floor(Math.random() * data.length);
+
+        $("#quote").html(data[randomNum].quote);
+        $("#author").html(data[randomNum].author);
+
+        var tweetQuote = '"' + data[randomNum].quote + '" - ' + data[randomNum].author;
+        tweetQuote = tweetQuote.split(" ").join("%20");
+        tweetQuote = "https://twitter.com/intent/tweet?text=" + tweetQuote;
+
+        $("#btweet").attr("href", tweetQuote);
+      });
+    }
+  );
+});
