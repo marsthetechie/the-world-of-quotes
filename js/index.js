7ad14cf496e7aaac07db26e1029e74918eb2ce64
Marijana Zlatkovic <m.zlatkovic@ymail.com>
2018-08-26 19:59:49 +0200

    updated gist link

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $.getJSON(
-    "https://gist.githubusercontent.com/mjanavic/3172b4f732eb8970fefa9ebd16149cfb/raw/17a539f37fcaf5b47547e6ea5a0a85e674ca1e09/quotes.json",
+    "https://gist.githubusercontent.com/marsthetechie/3172b4f732eb8970fefa9ebd16149cfb/raw/17a539f37fcaf5b47547e6ea5a0a85e674ca1e09/quotes.json",
     function (data) {
       $("#bquote").on("click", function () {
         var randomNum = Math.floor(Math.random() * data.length);
